1d85ecf829b6af74307beb65f744b6d5deff10d0
Carol Enache <enache.carol@gmail.com>
2017-11-22 13:32:48 +0000

    Turn the Square component into a  CONTROLLED COMPONENT that informs his parent, Board about any changes

--- src/index.js
+++ src/index.js
@@ -4,18 +4,11 @@ import './index.css';
 
 
 class Square extends Component{
-    constructor(props){
-        super(props);
-        this.state={
-            value: null
-        }
-    }
-
 
     render(){
         return (
-            <button className='square' onClick={ () => this.setState({value: 'X'}) }>
-                {this.state.value}
+            <button className='square' onClick={ () => this.props.onClick() }>
+                {this.props.value}
             </button>
         );
     }
@@ -30,8 +23,17 @@ class Board extends Component{
         };
     }
 
+    handleClick(i){
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
     renderSquare(i){
-        return <Square value={this.state.squares[i]}/>;
+        return <Square 
+                    value={this.state.squares[i]}
+                    onClick={() => this.handleClick(i)}
+                    />;
     }
 
     render(){
